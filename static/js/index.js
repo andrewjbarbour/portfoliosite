f1b44eaf863ef10d7656e34373d1ce87c5a6813c
Andrew Barbour <barbouraj@gmail.com>
2021-04-14 16:46:45 -0700

    built responsive popout nav menu

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 const root = document.querySelector('html');
 const body = document.querySelector('body');
-const backToTopLink = document.getElementById("back-to-top-link")
+const backToTopLink = document.getElementById("back-to-top-link");
+const navPopout = document.getElementById('popout-nav-menu');
 
 const scrollToTop = () =>{
     window.scrollTo(0, 0);
@@ -10,3 +11,18 @@ backToTopLink.addEventListener('click', ()=> {
     scrollToTop();
 })
 
+const toggleNavMenu = () => {
+    const navMenu = document.getElementById('popout-nav-menu-expanded');
+    const overlay = document.getElementById('overlay');
+    navMenu.style.display = "block";
+    overlay.style.display = "block";
+}
+
+navPopout.addEventListener('click', () => {toggleNavMenu()});
+
+const closePopout = () => {
+    const navMenu = document.getElementById('popout-nav-menu-expanded');
+    const overlay = document.getElementById('overlay');
+    navMenu.style.display = "none";
+    overlay.style.display = "none";
+}
