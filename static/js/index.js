1471fe23d67d10a3f710d7ba6aef3785fe95e776
Andrew Barbour <barbouraj@gmail.com>
2021-04-16 10:08:11 -0700

    optimized JS and CSS performance

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
-const root = document.querySelector('html');
-const body = document.querySelector('body');
 const backToTopLink = document.getElementById("back-to-top-link");
 const navPopout = document.getElementById('popout-nav-menu');
+const overlay = document.getElementById('overlay');
+const closePopoutIcon = document.getElementById('close-popout-icon');
 
 const scrollToTop = () =>{
     window.scrollTo(0, 0);
@@ -13,16 +13,16 @@ backToTopLink.addEventListener('click', ()=> {
 
 const toggleNavMenu = () => {
     const navMenu = document.getElementById('popout-nav-menu-expanded');
-    const overlay = document.getElementById('overlay');
     navMenu.style.display = "block";
     overlay.style.display = "block";
 }
 
-navPopout.addEventListener('click', () => {toggleNavMenu()});
-
 const closePopout = () => {
     const navMenu = document.getElementById('popout-nav-menu-expanded');
-    const overlay = document.getElementById('overlay');
     navMenu.style.display = "none";
     overlay.style.display = "none";
 }
+
+navPopout.addEventListener('click', () => {toggleNavMenu()});
+overlay.addEventListener('click', () => {closePopout()});
+closePopoutIcon.addEventListener('click', () => {closePopout()});
