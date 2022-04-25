d5fa8f190fac6bb14d51a121ec39624f3b176da4
Andrew Barbour <barbouraj@gmail.com>
2022-04-24 18:07:10 -0700

    update resume and dynamically set current copyright date with JS

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,37 @@
 const backToTopLink = document.getElementById("back-to-top-link");
-const navPopout = document.getElementById('popout-nav-menu');
-const overlay = document.getElementById('overlay');
-const closePopoutIcon = document.getElementById('close-popout-icon');
+const navPopout = document.getElementById("popout-nav-menu");
+const overlay = document.getElementById("overlay");
+const closePopoutIcon = document.getElementById("close-popout-icon");
 
-const scrollToTop = () =>{
-    window.scrollTo(0, 0);
-}
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
 
-backToTopLink.addEventListener('click', ()=> {
-    scrollToTop();
-})
+backToTopLink.addEventListener("click", () => {
+  scrollToTop();
+});
 
 const toggleNavMenu = () => {
-    const navMenu = document.getElementById('popout-nav-menu-expanded');
-    navMenu.style.display = "block";
-    overlay.style.display = "block";
-}
+  const navMenu = document.getElementById("popout-nav-menu-expanded");
+  navMenu.style.display = "block";
+  overlay.style.display = "block";
+};
 
 const closePopout = () => {
-    const navMenu = document.getElementById('popout-nav-menu-expanded');
-    navMenu.style.display = "none";
-    overlay.style.display = "none";
-}
+  const navMenu = document.getElementById("popout-nav-menu-expanded");
+  navMenu.style.display = "none";
+  overlay.style.display = "none";
+};
 
-navPopout.addEventListener('click', () => {toggleNavMenu()});
-overlay.addEventListener('click', () => {closePopout()});
-closePopoutIcon.addEventListener('click', () => {closePopout()});
+navPopout.addEventListener("click", () => {
+  toggleNavMenu();
+});
+overlay.addEventListener("click", () => {
+  closePopout();
+});
+closePopoutIcon.addEventListener("click", () => {
+  closePopout();
+});
+
+const year = document.querySelector(".year");
+year.textContent = new Date().getFullYear();
